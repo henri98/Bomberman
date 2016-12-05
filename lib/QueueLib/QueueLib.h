ec61eecab6f14960169aa409bb735ce09c72b51a
Neejij <s1016933@student.windesheim.nl>
2016-12-05 14:21:41 +0100

    Added Queue library

---
+++ lib/QueueLib/QueueLib.h
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TRUE  1
+#define FALSE	0
+
+/* a link in the queue, holds the info and point to the next Node*/
+typedef struct {
+	int info;
+} DATA;
+
+typedef struct Node_t {
+	DATA data;
+	struct Node_t *prev;
+} NODE;
+
+/* the HEAD of the Queue, hold the amount of node's that are in the queue*/
+typedef struct Queue {
+	NODE *head;
+	NODE *tail;
+	int size;
+	int limit;
+} Queue;
+
+Queue *ConstructQueue(int limit);
+void DestructQueue(Queue *queue);
+int Enqueue(Queue *pQueue, NODE *item);
+NODE *Dequeue(Queue *pQueue);
+int isEmpty(Queue* pQueue);
