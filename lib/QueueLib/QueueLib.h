612d48a5503c076f10f2c0be63f9a50e22e18e7a
Jeroen Kooiker <jkooiker.jk@gmail.com>
2016-12-07 11:59:17 +0100

    Queues again ..

--- lib/QueueLib/QueueLib.h
+++ lib/QueueLib/QueueLib.h
@@ -3,20 +3,20 @@
 
 /* a link in the queue, holds the info and point to the next Node*/
 typedef struct {
-	char byte;
+  unsigned char byte;
 } DATA;
 
 typedef struct Node_t {
-	DATA data;
-	struct Node_t *prev;
+  DATA data;
+  struct Node_t *prev;
 } NODE;
 
 /* the HEAD of the Queue, hold the amount of node's that are in the queue*/
 typedef struct Queue {
-	NODE *head;
-	NODE *tail;
-	int size;
-	int limit;
+  NODE *head;
+  NODE *tail;
+  int size;
+  int limit;
 } Queue;
 
 Queue *ConstructQueue(int limit);
